091dd47b75aa1319a1121f2766ba6f5ab54a733a
Justin Hibbits <chmeeedalf@gmail.com>
2020-09-12 23:32:57 -0500

    Set default SPE FP environment

--- lib/msun/powerpc/fenv.c
+++ lib/msun/powerpc/fenv.c
@@ -35,11 +35,19 @@
 #include <machine/spr.h>
 #endif
 
+#ifdef __SPE__
+#include <machine/spr.h>
+#endif
+
 #ifdef __GNUC_GNU_INLINE__
 #error "This file must be compiled with C99 'inline' semantics"
 #endif
 
+#ifdef __SPE__
+const fenv_t __fe_dfl_env = SPEFSCR_DFLT;
+#else
 const fenv_t __fe_dfl_env = 0x00000000;
+#endif
 
 extern inline int feclearexcept(int __excepts);
 extern inline int fegetexceptflag(fexcept_t *__flagp, int __excepts);
